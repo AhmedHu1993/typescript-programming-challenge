edfd4f6e0067a29d0618dc2dfc1a41e05195d676
Ahmed Hussein <ahussein@podfather.com>
2022-05-16 00:09:50 +0100

    fix second question test cases

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+import {readFileSync} from "fs";
+
 export async function solveFirstQuestion(
   inputFilePath: string
 ): Promise<string> {
-  const fs = require('fs');
-  const dataArray: string[] = fs.readFileSync(inputFilePath).toString().split("\n");
+  const dataArray: string[] = readFileSync(inputFilePath).toString().split("\n");
 
   let earliest: Date = new Date('2030-12-10T12:00:00')
-  for(let interval of dataArray) {
+  for (let interval of dataArray) {
     const datesString: string = interval.substring(interval.indexOf('[') + 1, interval.indexOf(']'));
     const dateArray: string[] = datesString.split(',');
 
@@ -25,8 +26,24 @@ export async function solveFirstQuestion(
 export async function solveSecondQuestion(
   inputFilePath: string
 ): Promise<string> {
-  // TODO: Solve me!
-  return "";
+  const dataArray: string[] = readFileSync(inputFilePath).toString().split("\n");
+
+  let latest: Date = new Date('1990-12-10T12:00:00')
+  for (let interval of dataArray) {
+    const datesString: string = interval.substring(interval.indexOf('[') + 1, interval.indexOf(']'));
+    const dateArray: string[] = datesString.split(',');
+
+    for (let date of dateArray) {
+      const dateString: string = date.substring(date.indexOf('/') + 1);
+      const intervalDate: Date = new Date(dateString);
+
+      if (intervalDate > latest) {
+        latest = intervalDate;
+      }
+    }
+  }
+
+  return latest.toISOString();
 }
 
 export async function solveThirdQuestion(
