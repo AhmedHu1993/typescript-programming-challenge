efc0d9da32bf12b92b9e6718f3f13946753251db
Ahmed Hussein <ahussein@podfather.com>
2022-05-15 23:47:56 +0100

    fixed first question

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 export async function solveFirstQuestion(
   inputFilePath: string
 ): Promise<string> {
-  // TODO: Solve me!
-  return "";
+  const fs = require('fs');
+  const dataArray: string[] = fs.readFileSync(inputFilePath).toString().split("\n");
+
+  let earliest: Date = new Date('2030-12-10T12:00:00')
+  for(let interval of dataArray) {
+    const datesString: string = interval.substring(interval.indexOf('[') + 1, interval.indexOf(']'));
+    const dateArray: string[] = datesString.split(',');
+
+    for (let date of dateArray) {
+      const dateString: string = date.substring(0, date.indexOf('/'));
+      const intervalDate: Date = new Date(dateString);
+
+      if (intervalDate < earliest) {
+        earliest = intervalDate;
+      }
+    }
+  }
+
+  return earliest.toISOString();
 }
 
 export async function solveSecondQuestion(
