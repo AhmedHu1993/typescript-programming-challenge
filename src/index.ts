d9a8703ad2877138f73bdc5b520e4b124775be72
Ahmed Hussein <ahussein@podfather.com>
2022-05-24 00:41:30 +0100

    add descriptive comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -54,21 +54,29 @@ export async function solveThirdQuestion(
 
   let allIntervals: DateInterval[][] = [];
   for (let interval of dataArray) {
+    // Getting all dates as a string in the interval
     const datesString: string = interval.substring(interval.indexOf('[') + 1, interval.indexOf(']'));
+    // From String to Dates Array
     const dateArray: string[] = datesString.split(',');
 
     let workerIntervals: DateInterval[] = []
     for (let date of dateArray) {
+      // Get the start date for each interval
       const startDateString: string = date.substring(0, date.indexOf('/'));
+      // Get the end date of each interval
       const endDateString: string = date.substring(date.indexOf('/') + 1);
 
+      /* Create a new DateInterval Class to hold the start date and the end date,
+      and to ease the overlap between the intervals */
       const newDateInterval: DateInterval = new DateInterval(startDateString, endDateString)
       workerIntervals.push(newDateInterval)
     }
+    // Create an array of dataIntervals array, to match the number of workers' interval.
     allIntervals.push(workerIntervals)
   }
 
-  const result = DateInterval.overlapAll(allIntervals);
-
+  // Get the result as array of DataInterval array
+  const result: DateInterval[] = DateInterval.overlapAll(allIntervals);
+  // Cast the DateInterval to startDate/endDate string
   return result.map(dataInterval => `${dataInterval.startDate}/${dataInterval.endDate}`);
 }
